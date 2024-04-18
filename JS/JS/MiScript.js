918486987d6ac29325522ced1b99006b7ff65479
juanAntonioMurillo <116297518+juanAntonioMurillo@users.noreply.github.com>
2024-04-18 10:59:09 -0700

    practica 1 js
    inicio de js y practica de  contador con localstora, tambien arrastrary soltar

---
+++ JS/JS/MiScript.js
@@ -0,0 +1,21 @@
+
+//mutable //cambia
+var a = 0;
+//usar mas let
+//solo funciona en donde la declares, dentro de un ciclo
+let b = 0; //scope - alcance - ambito
+//consemto de inmutable //no camboa
+const pi = 3.1416;
+alert("JS EN Proceso");
+
+function boton(){
+    alert("Hay Un Chingoooo deeee pornooooooo");
+    console.log("La Base De Viruz Asido Actualizada");
+}
+
+const flecha = ()=>{//arrow Function
+    alert("Funcion Arrow Function - anonima");
+    return "Valor De rectorno"
+}
+//colva esperar que conteste
+   
