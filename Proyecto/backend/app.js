cdca4803838f5ac10dbc935b1b9a27a1530e084b
juanAntonioMurillo <116297518+juanAntonioMurillo@users.noreply.github.com>
2024-04-25 10:50:57 -0700

    Conecion a Mysql
    colocamos lso get, coneciotn de para mysl, y consultamos a la base de datos los clientes

--- Proyecto/backend/app.js
+++ Proyecto/backend/app.js
@@ -6,15 +6,43 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+// Conexion a Mysql
+let conexion = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a Mysql
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log("conecto ala ase de datos");
+    }
+});
+
 //Rutas De Acceso
 app.get("/",function(req,res){
     res.send("Ruta De Inicio");
 })
+ //seleccionamos todo los clientes
+app.get("/api/clientes",(req,res)=>{
+    conexion.query('select * from clietes', (error,regristos)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(regristos);
+        }
 
+    });
+});
 
 
 //Encender Servidor
 let puerto = 3000;
 app.listen(puerto, function(){
-    console.log('SErvidor escuchando puerto ' + puerto);
+    console.log('Servidor escuchando puerto ' + puerto);
 })
