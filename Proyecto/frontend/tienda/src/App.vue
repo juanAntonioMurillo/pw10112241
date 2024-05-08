37766faab53dc33ccc44ed06029f4375ea22c7e1
juanAntonioMurillo <116297518+juanAntonioMurillo@users.noreply.github.com>
2024-05-08 11:00:50 -0700

    Menu View
    creamos la carpeta fronted y istalamos vue

---
+++ Proyecto/frontend/tienda/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import MenuView from './views/MenuView.vue'
+</script>
+
+<template>
+  <header>
+    <h1>HelloWorld</h1>
+    <MenuView/>
+    
+  </header>
+
+  <RouterView />
+</template>
+
